a71e4afe4c342ff9afabf52dd7c4bbb2894316ee
greedval-us <valkirblad@gmail.com>
2026-05-21 16:09:13 +0300

    greedval

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,9 +3,11 @@
 use App\Providers\AppServiceProvider;
 use App\Providers\FortifyServiceProvider;
 use App\Providers\MadelineProtoServiceProvider;
+use App\Providers\MoonShineServiceProvider;
 
 return [
     AppServiceProvider::class,
     FortifyServiceProvider::class,
     MadelineProtoServiceProvider::class,
+    MoonShineServiceProvider::class,
 ];
