a764ed5d9ebb9fe2c36135007030f4cd34151c68
greedval-us <valkirblad@gmail.com>
2026-06-10 17:11:15 +0300

    greedval

--- resources/js/layouts/app/AppSidebarLayout.vue
+++ resources/js/layouts/app/AppSidebarLayout.vue
@@ -3,6 +3,7 @@ import AppContent from '@/components/AppContent.vue';
 import AppShell from '@/components/AppShell.vue';
 import AppSidebar from '@/components/AppSidebar.vue';
 import AppSidebarHeader from '@/components/AppSidebarHeader.vue';
+import { SidebarTrigger } from '@/components/ui/sidebar';
 import type { BreadcrumbItem } from '@/types';
 
 type Props = {
@@ -16,7 +17,12 @@ withDefaults(defineProps<Props>(), {
 
 <template>
     <AppShell variant="sidebar">
-        <AppSidebar />
+        <div class="relative">
+            <AppSidebar />
+            <SidebarTrigger
+                class="absolute top-4 -right-3 z-30 rounded-full border border-sidebar-border bg-background shadow-sm"
+            />
+        </div>
         <AppContent
             variant="sidebar"
             class="flex min-h-0 flex-1 overflow-hidden"
